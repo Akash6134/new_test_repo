8bf275924b6265559bee25f485bfddbfe2a30f33
Akash Kumar <akashk.20mt0031@mme.iitism.ac.in>
2022-12-12 00:53:20 +0530

    1st repo commit

---
+++ new_test_repo/README.md
@@ -0,0 +1,5 @@
+# Instruction how to use akash_iq_library module
+
+## Selection telling how to install the module
+
+<p>Some text explaining how to install the module to Python Environment.</p>
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='new_test_repo',
+      version='0.1',
+      description='contains many data science modules',
+      author='Akash Kumar',
+      packages=['hello_world'],
+      zip_safe=False)
